fc486ff03b01a7d40986f5565b3f85536f987e4b
Yulia Boiko <yulchitaiboldireva@gmail.com>
2021-10-31 22:15:09 +0200

    make logo as link to home, fix modal scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
                 <nav className={styles.nav}>
-                    <img className={styles.logo} src="img/logo2.png" alt="pets store logo"/>
+                    <Link exact to="/"><img className={styles.logo} src="img/logo2.png" alt="pets store logo" /></Link>
                     <ul className={styles.navList}>
                         <li className={styles.item}><NavLink exact to="/" activeClassName={styles.active}>Home</NavLink></li>
                         <li className={styles.item}><NavLink exact to="/cart" activeClassName={styles.active}>Cart</NavLink></li>
